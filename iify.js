aa91ea12d4b95a7b4159a739e7a7d5ddb1ab25a2
Ali Muhammadi <ali@trade2simple>
2024-03-26 14:21:41 +0500

    iify practise

---
+++ iify.js
@@ -0,0 +1,37 @@
+// stander module pattern
+
+// const uictrl = (() => {
+//   let text = "hellow world";
+//   const getText = () => {
+//     const element = document.querySelector("h1");
+//     element.textContent = text;
+//   };
+//   return {
+//     callChangerText: () => {
+//       getText();
+//       //   console.log(text);
+//     },
+//   };
+// })();
+
+// uictrl.callChangerText();
+// // uictrl.getText();
+// console.log(uictrl.text);
+
+const itemCtrl = (() => {
+  let data = [];
+  add = (item) => {
+    data.push(item);
+    console.log("item added....");
+  };
+  function get(id) {
+    return data.find((v) => v.id === id);
+  }
+  return {
+    add: add,
+    // get: get,
+  };
+})();
+itemCtrl.add({ id: 1, name: "jhn" });
+itemCtrl.add({ id: 2, name: "weak" });
+console.log(itemCtrl.get(2));
